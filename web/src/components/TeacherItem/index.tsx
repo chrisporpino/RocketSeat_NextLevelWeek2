5a4a9ff3609b1b5b0b6313c46ec5131f628765dc
Christhopher Porpino <cgporpinof@gmail.com>
2020-08-19 13:43:49 -0300

    finalizando dia3

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,33 +3,55 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css'
+import api from '../../services/api';
 
-const TeacherItem: React.FC = () => {
+export interface Teacher {
+  id: number;
+  subject: string;
+  cost: number;
+  name: string;
+  avatar: string;
+  whatsapp: string;
+  bio: string;
+}
+
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id,
+    })
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://avatars3.githubusercontent.com/u/53712536?s=460&u=8fce4248cd2ecbda14c8bf3f3b649bc07d3d47c6&v=4" alt="Rosana Rezende" />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Christhopher Porpino</strong>
-          <span>Química</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-        <br /><br />
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem delectus quod officiis, quisquam a ullam. Excepturi iure perferendis illo error delectus, cupiditate tempora exercitationem quis odio voluptatum reiciendis culpa assumenda!
-      </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 20,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a
+          target="_blank"
+          onClick={createNewConnection}
+          href={`https://wa.me/${teacher.whatsapp}?text=Oi%21%20Eu%20gostaria%20de%20marcar%20uma%20aula&source=&data=&app_absent=`}
+        >
           <img src={whatsappIcon} alt="Whatsapp" />
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
